aaebcced142d45b772d2e0be840ae9b31384e451
Naman <naman@MacBookPro.lan>
2025-08-14 09:35:28 +0530

    String methods

--- Core/Basics/src/datatypes/Test.java
+++ Core/Basics/src/datatypes/Test.java
@@ -1,16 +1,57 @@
 package datatypes;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class Test {
 
     public static void main(String [] args){
-        int a =1;
-        Student student = new Student(); //stored in heap memory.
-        student.address  = "Gwalior";
-        student.name = "Naman";
-        student.standard = 4;
-        student.rollNumber = 123432;
+        String name1 = "Akshit Sharma";
+        int length= name1.length();
 
 
-        System.out.println(student.name);
+//    How to check the equality of the string .
+        String name2 = "akshit Sharma";
+
+//        System.out.println(name1.equals(name2));
+
+//        but you also have to ignore the upper case letter in that case;
+
+//        System.out.println(name1.equalsIgnoreCase(name2));
+/*
+        String str1 = "remote";
+        String str2 = "car";
+
+        int i = str1.compareTo(str2);
+        System.out.println(i);
+            */
+
+        /* String name =  "Naman Rao";
+
+        String string = name.substring(0,4); //last range is not included.
+        System.out.println(string);
+        */
+
+
+        /*String name = "Naman Rao"; //Remove the extra space in the string.
+        //Original string remains unchanged because it is immutable.
+        System.out.println(name.trim());
+        String newName = name.replace("Naman", "Ravi");
+        System.out.println(newName);
+            */
+
+        /*
+        int a = 10;
+        String s = String.valueOf(a);
+        System.out.println(s);
+        */
+
+        String formattedStr = String.format("My name is %s and I am %d years old.", "John",25);
+        System.out.println(formattedStr);
+
+        String name = "Amar Panchal";
+        System.out.println(name.substring(2,8));
+        System.out.println(name.subSequence(2,8));
+
+
     }
 }
--- Core/Basics/src/datatypes/TestString.java
+++ Core/Basics/src/datatypes/TestString.java
@@ -2,13 +2,6 @@ package datatypes;
 
 public class TestString {
     public static void main(String[] args) {
-        String x = "Ram";
-        String a = new String(x);
-        String b = new String(x);
-        String c = "Ram";
-        String d= "Ram";
-
-        System.out.println(a ==b); //here we are checking the reference from just ==.
-        System.out.println(c ==d);
+        String name = "Ram";
     }
 }
