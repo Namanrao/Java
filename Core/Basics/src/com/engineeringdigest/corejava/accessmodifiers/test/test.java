4210c1059d007cf82bc7ff29b7e28f56d906e300
Naman <naman@MacBookPro.lan>
2025-08-19 20:24:31 +0530

    SpringBoot Folder

--- Core/Basics/src/com/engineeringdigest/corejava/accessmodifiers/test/test.java
+++ Core/Basics/src/com/engineeringdigest/corejava/accessmodifiers/test/test.java
@@ -1,11 +1,8 @@
 package com.engineeringdigest.corejava.accessmodifiers.test;
 
-import com.engineeringdigest.corejava.accessmodifiers.school.Student;
 
 public class test {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.age = 12;
-        student.name = "Ram";
+
     }
 }
---
+++ Core/Basics/src/com/engineeringdigest/corejava/test/Duck.java
@@ -0,0 +1,11 @@
+package com.engineeringdigest.corejava.test;
+
+public class Duck extends Animal {
+
+
+    public Duck(String name, int age) {
+        super(name, age);
+    }
+    //    constructor creation
+
+}
